1198a659cb06493d2d555273432531f6cdd4f1ff
23ohyeah <146018591+23ohyeah@users.noreply.github.com>
2024-03-18 12:00:30 -0500

    Add files via upload

---
+++ Projects/Lessons/Stuff/Calculator/calculator.js
@@ -0,0 +1,49 @@
+document.getElementById("num1").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "1";
+    }
+
+document.getElementById("num2").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "2";
+    }
+
+document.getElementById("num3").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "3";
+    }
+
+document.getElementById("num4").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "4";
+    }
+
+document.getElementById("num5").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "5";
+    }
+
+document.getElementById("num6").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "6";
+    }
+
+document.getElementById("num7").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "7";
+    }
+
+document.getElementById("num8").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "8";
+    }
+
+document.getElementById("num9").oneclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "9";
+    }    
+
+document.getElementById("clear").oneclick = function writeClear()
+    {
+        document.getElementById("screen").innerHTML = "";
+    }
